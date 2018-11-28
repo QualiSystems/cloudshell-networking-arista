35a35609972bbdbb72b78d33279f48f70705421b
Kyrylo Maksymenko <saklar13@gmail.com>
2018-11-28 13:46:03 +0200

    update tests

--- cloudshell/networking/arista/flows/arista_restore_flow.py
+++ cloudshell/networking/arista/flows/arista_restore_flow.py
@@ -1,11 +1,8 @@
-from collections import OrderedDict
-
 from cloudshell.devices.flows.action_flows import RestoreConfigurationFlow
 from cloudshell.networking.arista.command_actions.system_actions import SystemActions
 
 
 class AristaRestoreFlow(RestoreConfigurationFlow):
-    STARTUP_LOCATION = "nvram:startup_config"
 
     def execute_flow(self, path, configuration_type, restore_method, vrf_management_name):
         """ Execute flow which save selected file to the provided destination
@@ -17,34 +14,15 @@ class AristaRestoreFlow(RestoreConfigurationFlow):
         :param vrf_management_name: Virtual Routing and Forwarding Name
         """
 
-        if "-config" not in configuration_type:
-            configuration_type += "-config"
+        if '-config' not in configuration_type:
+            configuration_type += '-config'
 
         with self._cli_handler.get_cli_service(self._cli_handler.enable_mode) as enable_session:
             restore_action = SystemActions(enable_session, self._logger)
             copy_action_map = restore_action.prepare_action_map(path, configuration_type)
 
-            if "startup" in configuration_type:
-                if restore_method == "override":
-                    del_action_map = OrderedDict({
-                        "[Dd]elete [Ff]ilename ": lambda session, logger: session.send_line(configuration_type,
-                                                                                            logger)})
-                    restore_action.delete_file(path=self.STARTUP_LOCATION, action_map=del_action_map)
-                    restore_action.copy(path,
-                                        configuration_type,
-                                        vrf=vrf_management_name,
-                                        action_map=copy_action_map)
-                else:
-                    restore_action.copy(path,
-                                        configuration_type,
-                                        vrf=vrf_management_name,
-                                        action_map=copy_action_map)
-
-            elif "running" in configuration_type:
-                if restore_method == "override":
-                    restore_action.override_running(path)
-                else:
-                    restore_action.copy(path,
-                                        configuration_type,
-                                        vrf=vrf_management_name,
-                                        action_map=copy_action_map)
+            if 'running' in configuration_type and restore_method == 'override':
+                restore_action.override_running(path)
+            else:
+                restore_action.copy(
+                    path, configuration_type, vrf_management_name, action_map=copy_action_map)
--- tests/networking/arista/test_restore_config.py
+++ tests/networking/arista/test_restore_config.py
@@ -22,7 +22,7 @@ class TestRestoreConfig(BaseAristaTestCase):
 
     @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
     @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
-    def test_restore_anonymous(self, send_mock, recv_mock):
+    def test_restore_running_override(self, send_mock, recv_mock):
         self._setUp()
         host = '192.168.122.10'
         file_name = 'Test-running-100418-163658'
@@ -30,21 +30,7 @@ class TestRestoreConfig(BaseAristaTestCase):
         configuration_type = 'running'
 
         emu = CliEmulator([
-            Command(
-                'configure replace {}'.format(remote_path),
-                'This operation will overwrite the current setting and could possibly '
-                'reboot the system!\n'
-                'Do you want to continue? (y/n)'
-            ),
-            Command(
-                'y',
-                '\n'
-                'Please wait...\n'
-                'Connect to ftp server 192.168.42.102 ...\n'
-                'Get config file from ftp server OK.\n'
-                'File check OK.\n'
-                '{}'.format(ENABLE_PROMPT),
-            ),
+            Command('configure replace {}'.format(remote_path), ENABLE_PROMPT)
         ])
         send_mock.side_effect = emu.send_line
         recv_mock.side_effect = emu.receive_all
@@ -53,166 +39,48 @@ class TestRestoreConfig(BaseAristaTestCase):
 
         emu.check_calls()
 
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
-    # def test_restore_ftp(self, send_mock, recv_mock):
-    #     host = '192.168.122.10'
-    #     file_name = 'Test-running-100418-163658'
-    #     user = 'user'
-    #     password = 'password'
-    #     remote_path = 'ftp://{}:{}@{}/{}'.format(user, password, host, file_name)
-    #     configuration_type = 'running'
-    #
-    #     emu = CliEmulator([
-    #         Command(
-    #             'execute restore config ftp {} {} {} {}'.format(file_name, host, user, password),
-    #             'This operation will overwrite the current setting and could possibly '
-    #             'reboot the system!\n'
-    #             'Do you want to continue? (y/n)'
-    #         ),
-    #         Command(
-    #             'y',
-    #             '\n'
-    #             'Please wait...\n'
-    #             'Connect to ftp server 192.168.42.102 ...\n'
-    #             'Get config file from ftp server OK.\n'
-    #             'File check OK.\n'
-    #             '{}'.format(ENABLE_PROMPT),
-    #         )
-    #     ])
-    #     send_mock.side_effect = emu.send_line
-    #     recv_mock.side_effect = emu.receive_all
-    #
-    #     self.runner.restore(remote_path, configuration_type)
-    #
-    #     emu.check_calls()
-    #
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
-    # def test_fail_to_restore(self, send_mock, recv_mock):
-    #     host = '192.168.122.10'
-    #     file_name = 'Test-running-100418-163658'
-    #     remote_path = 'ftp://{}/{}'.format(host, file_name)
-    #     configuration_type = 'running'
-    #
-    #     emu = CliEmulator([
-    #         Command(
-    #             'execute restore config ftp {} {}'.format(file_name, host),
-    #             'This operation will overwrite the current setting and could possibly '
-    #             'reboot the system!\n'
-    #             'Do you want to continue? (y/n)'
-    #         ),
-    #         Command(
-    #             'y',
-    #             '\n'
-    #             'Please wait...\n'
-    #             'Connect to ftp server 192.168.122.240 ...\n'
-    #             'Can not get config file from ftp server via vdom root. Err code: 5.\n'
-    #             'Command fail. Return code -28\n'
-    #             '\n'
-    #             '{}'.format(ENABLE_PROMPT),
-    #         )
-    #     ])
-    #     send_mock.side_effect = emu.send_line
-    #     recv_mock.side_effect = emu.receive_all
-    #
-    #     self.assertRaisesRegexp(
-    #         Exception,
-    #         'Session returned \'Fail to restore config\'',
-    #         self.runner.restore,
-    #         remote_path,
-    #         configuration_type,
-    #     )
-    #
-    #     emu.check_calls()
-    #
-    # def test_append_method(self):
-    #     remote_path = 'ftp://user:password@192.168.122.10/Test-running-100418-163658'
-    #     configuration_type = 'running'
-    #     restore_method = 'append'
-    #
-    #     self.assertRaisesRegexp(
-    #         FortiNetException,
-    #         'The device doesn\'t support append restore method',
-    #         self.runner.restore,
-    #         remote_path,
-    #         configuration_type,
-    #         restore_method,
-    #     )
-    #
-    # def test_restore_startup(self):
-    #     remote_path = 'ftp://user:password@192.168.122.10/Test-startup-100418-163658'
-    #     configuration_type = 'startup'
-    #
-    #     self.assertRaisesRegexp(
-    #         FortiNetException,
-    #         'The device doesn\'t support startup configuration',
-    #         self.runner.restore,
-    #         remote_path,
-    #         configuration_type,
-    #     )
-    #
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
-    # def test_restore_from_device(self, send_mock, recv_mock):
-    #     configuration_type = 'running'
-    #
-    #     emu = CliEmulator([
-    #         Command(
-    #             'execute revision list config',
-    #             'Last Firmware Version: V0.0.0-build000-REL0\n'
-    #             'ID TIME                   ADMIN                	FIRMWARE VERSION     	COMMENT\n'
-    #             ' 4 2018-10-08 11:54:28    admin                	V6.0.0-build163-REL0 	Test-running-081018-215424\n'
-    #             ' 6 2018-10-08 12:33:47    admin                	V6.0.0-build163-REL0 	Test-running-081018-223342\n'
-    #             '{}'.format(ENABLE_PROMPT),
-    #         ),
-    #         Command(
-    #             'execute restore config flash 4',
-    #             'This operation will overwrite the current setting and could possibly '
-    #             'reboot the system!\n'
-    #             'Do you want to continue? (y/n)'
-    #         ),
-    #         Command(
-    #             'y',
-    #             '\n'
-    #             'Please wait...\n'
-    #             'Get config from local disk OK.\n'
-    #             'File check OK.\n'
-    #             '{}'.format(ENABLE_PROMPT),
-    #         ),
-    #     ])
-    #     send_mock.side_effect = emu.send_line
-    #     recv_mock.side_effect = emu.receive_all
-    #
-    #     self.runner.restore('', configuration_type)
-    #
-    #     emu.check_calls()
-    #
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
-    # @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
-    # def test_restore_from_device_not_exists_config(self, send_mock, recv_mock):
-    #     path = 'flash://Test-running-081018-215425'
-    #     configuration_type = 'running'
-    #
-    #     emu = CliEmulator([
-    #         Command(
-    #             'execute revision list config',
-    #             'Last Firmware Version: V0.0.0-build000-REL0\n'
-    #             'ID TIME                   ADMIN                	FIRMWARE VERSION     	COMMENT\n'
-    #             ' 4 2018-10-08 11:54:28    admin                	V6.0.0-build163-REL0 	Test-running-081018-215424\n'
-    #             ' 6 2018-10-08 12:33:47    admin                	V6.0.0-build163-REL0 	Test-running-081018-223342\n'
-    #             '{}'.format(ENABLE_PROMPT),
-    #         ),
-    #     ])
-    #     send_mock.side_effect = emu.send_line
-    #     recv_mock.side_effect = emu.receive_all
-    #
-    #     self.assertRaisesRegexp(
-    #         FortiNetException,
-    #         'Cannot find config file ".*?" on the device',
-    #         self.runner.restore,
-    #         path,
-    #         configuration_type,
-    #     )
-    #
-    #     emu.check_calls()
+    @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
+    @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
+    def test_restore_running_append(self, send_mock, recv_mock):
+        self._setUp()
+        host = '192.168.122.10'
+        file_name = 'Test-running-100418-163658'
+        remote_path = 'ftp://{}/{}'.format(host, file_name)
+        configuration_type = 'running'
+
+        emu = CliEmulator([
+            Command(
+                'copy {} {}-config'.format(remote_path, configuration_type),
+                'Copy completed successfully.\n'
+                '{}'.format(ENABLE_PROMPT),
+            )
+        ])
+        send_mock.side_effect = emu.send_line
+        recv_mock.side_effect = emu.receive_all
+
+        self.runner.restore(remote_path, configuration_type, 'append')
+
+        emu.check_calls()
+
+    @patch('cloudshell.cli.session.ssh_session.SSHSession._receive_all')
+    @patch('cloudshell.cli.session.ssh_session.SSHSession.send_line')
+    def test_restore_startup(self, send_mock, recv_mock):
+        self._setUp()
+        host = '192.168.122.10'
+        file_name = 'Test-startup-100418-163658'
+        remote_path = 'ftp://{}/{}'.format(host, file_name)
+        configuration_type = 'startup'
+
+        emu = CliEmulator([
+            Command(
+                'copy {} {}-config'.format(remote_path, configuration_type),
+                'Copy completed successfully.\n'
+                '{}'.format(ENABLE_PROMPT),
+            )
+        ])
+        send_mock.side_effect = emu.send_line
+        recv_mock.side_effect = emu.receive_all
+
+        self.runner.restore(remote_path, configuration_type)
+
+        emu.check_calls()
